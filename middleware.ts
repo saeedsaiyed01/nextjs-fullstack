212495c094227e184c5b405e8383ccab4327026d
SAEED <149547355+saeedsaiyed01@users.noreply.github.com>
2025-07-30 21:02:33 +0530

    small

--- middleware.ts
+++ middleware.ts
@@ -28,13 +28,7 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public folder
-     */
+    
     "/((?!_next/static|_next/image|favicon.ico|public/).*)",
   ],
 };
